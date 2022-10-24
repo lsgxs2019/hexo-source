8b5798ca52b90387b6ac0935f2893eae8866f620
lsgxs2019 <lsgxs141@126.com>
2022-10-24 09:10:14 +0800

    udpate

--- gitpush.bat
+++ gitpush.bat
@@ -3,31 +3,31 @@
 @echo===============================hexo-source-git-add-commit-push-start================================
 git add .
 @echo  git add . 添加文档完成
-@echo
-@echo
+@echo.
+@echo.
 git commit -m "udpate"
 @echo  git  commit 完成
-@echo 
-@echo
+@echo. 
+@echo.
 git  push origin 
 @echo==============================hexo-source-git-add-commit-push-successfully============================
-@echo 
-@echo
+@echo.
+@echo.
 @echo 当前路径%cd%
 pause 
 @echo 切换到public目录下
 cd c:\hexo-source\public
 @echo 当前路径%cd%
 pause
-@echo 
-@echo 
+@echo.
+@echo. 
 @echo=============================lsgxs2019.github.io-git-add-commit-push-start==============================
 git add .
 @echo  git add . 添加文档完成
-@echo 
+@echo. 
 git commit -m "udpate"
-@echo
+@echo.
 @echo  git  commit 完成
-@echo 
+@echo. 
 git  push origin 
 @echo=============================lsgxs2019.github.io-git-add-commit-push-successfully=========================
